e56adec2eb8e55dc4468bb7775ce88ebc8503037
pateldevang <devangpatel92@gmail.com>
2019-07-14 18:55:31 +0530

    Initial

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,7 +4,7 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!"
+        return "It worlking and Live!"
     }
     
     // Basic "Hello, world!" example
